4a7785e7bcf7bd240759edf605b322d752f88667
n.kamalov <n.kamalov@uic.group>
2026-02-24 11:07:57 +0500

    string methods2

---
+++ stringMethods2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class stringMethods2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+        scanner.close();
+
+        String[] listString = text.split(" ");
+        String joinedString = String.join(delimiter, listString);
+
+        System.out.println(joinedString);
+        
+    }
+}
